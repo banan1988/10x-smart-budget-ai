34c2878272b4a8b74d30fb6ddeb9bb56b0678f12
banan1988 <banan1988@users.noreply.github.com>
2025-11-15 21:00:16 +0100

    chore: moved DEFAULT_USER_ID to constants

---
+++ src/db/constants.ts
@@ -0,0 +1,10 @@
+/**
+ * Constants for development and testing purposes
+ */
+
+/**
+ * Temporary hardcoded user ID for development purposes.
+ * This should be replaced with proper authentication in production.
+ */
+export const DEFAULT_USER_ID = '3005cb66-fb36-4d74-be85-24c1eaa96e5f';
+
